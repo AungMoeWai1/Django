00b4b5f0aa1341533a02ff9293b8e88e48baa135
tkyawwai678@gmail.com <tkyawwai678@gmail.com>
2025-05-06 09:06:38 +0630

    Modify for the member app with test file

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
-  mymembers=Member.objects.all().values()
+  # mymembers=Member.objects.all().values()
+
+  #To filter for the specific member by using or operator  
+  mydata = Member.objects.filter(firstname='Emil').values() | Member.objects.filter(firstname='Tobias').values()
+  #Short format for the specific query
+  mydata = Member.objects.filter(Q(firstname='Tobias') | Q(firstname='Aung')).values()
+
   template = loader.get_template('all_members.html')
   context={
-    'mymembers':mymembers,
+    'mymembers':mydata,
   }
   return HttpResponse(template.render(context,request))
 
@@ -30,3 +37,7 @@ def testing(request):
     'greeting':1,
   }
   return HttpResponse(template.render(context,request))
+
+def footerTest(request):
+  template=loader.get_template("template.html")
+  return HttpResponse(template.render())
